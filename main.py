126780b9d8cf94f1a42644d510454a4cc7f9ad6d
akhundubedullah <akhundubedullah30@gmail.com>
2024-07-02 22:45:49 +0500

    add_task function

--- main.py
+++ main.py
@@ -66,53 +66,26 @@ def task_manager():
     add_button = tk.Button(task_window, text="Add Task", command=lambda: add_task(task_listbox, task_entry))
     add_button.pack()
 
-    delete_button = tk.button(task_window,text = "Add Task",command = lambda :delete_task(task_listbox))
+    delete_button = tk.button(task_window, text = "Delete Task",command = lambda :delete_task(task_listbox))
     delete_button.pack()
 
-    update_button = tk.Button(task_window,text = "Update Task",command = lambda :update_task(task_listbox,task_entry))
+    update_button = tk.Button(task_window, text = "Update Task",command = lambda :update_task(task_listbox,task_entry))
     update_button.pack()
 
 
-
-def add_task():
-
-    task = task_entry.get()
-    if task:
-        task.append(task)
-        
-
-
-
+    def add_task(task_listbox, task_entry):
+        task = task_entry.get()
+        if task:
+            task.append(task) # what will happen if i remove this
+            task_listbox.insert(tk.END, task)
+            task_entry.delete(0, tk.END)
+        else:
+            tk.messagebox.showwarning("Input Error", "Please enter a task.")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#def main():
-    #print("Hello")
-    #print("Enter 1 ii you want to SignUp")
-    #print("Enter 2 if you want to Login")
-
-    #number = input("Enter your desired Number: ")
-    #if(number == 1):
-    #    signup()
-
-    #elif(number == 2):
-    #    login()
-
-    #else:
-        #print("Please try again.")
-
-
-#main()
+    def delete_task(task_listbox):
+        try:
+            task_index = task_listbox.curselection()
+            task_listbox.delete(task_index)
+        except:
+            tk.messagebox.showwarning("Selection Error", "Please select a task to delete.")    
